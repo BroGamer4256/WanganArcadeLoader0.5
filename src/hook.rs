cb09376a63cd28974de38c54f0566be74a19674a
BroGamer <64546358+BroGamer4256@users.noreply.github.com>
2024-02-24 19:19:20 +1300

    Use retour instead of my own hacky solution

--- src/hook.rs
+++ src/hook.rs
@@ -1,11 +1,8 @@
-// Retour didnt work so im doing it all myself
-// This solution sucks btw
-
 use libc::*;
 use std::ffi::CString;
 
-pub unsafe fn hook_symbol(symbol: &str, func: *const ()) {
-	hook(get_symbol(symbol), func);
+pub unsafe fn hook_symbol(symbol: &str, func: *const ()) -> *const () {
+	hook(get_symbol(symbol), func)
 }
 
 pub unsafe fn get_symbol(symbol: &str) -> *mut () {
@@ -16,21 +13,10 @@ pub unsafe fn get_symbol(symbol: &str) -> *mut () {
 	address as *mut ()
 }
 
-pub unsafe fn hook(address: *mut (), func: *const ()) {
-	let func = func as usize;
-	let func = func.to_le_bytes();
-	/*
-		mov eax, func
-		jmp eax
-	*/
-	let mut data = Vec::with_capacity(7);
-	data.push(0xB8);
-	for i in func {
-		data.push(i);
-	}
-	data.push(0xFF);
-	data.push(0xE0);
-	write_memory(address, &data);
+pub unsafe fn hook(address: *mut (), func: *const ()) -> *const () {
+	let hook = retour::RawDetour::new(address, func).unwrap();
+	hook.enable().unwrap();
+	hook.trampoline()
 }
 
 pub unsafe fn write_memory(address: *mut (), data: &[u8]) {
