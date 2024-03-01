47354433b3d1d159c835cb59f1279663be56dea7
BroGamer <64546358+BroGamer4256@users.noreply.github.com>
2024-03-01 18:24:34 -0500

    Add back no_display

--- dist/start.sh
+++ dist/start.sh
@@ -39,5 +39,5 @@ if [ ! -f ./data/shader/.recompiled ]; then
 fi
 
 export LD_LIBRARY_PATH="${PWD};${PWD}/libso"
-export MANGOHUD_CONFIG="fps_limit=60;"
+export MANGOHUD_CONFIG="fps_limit=60;no_display=1"
 LD_PRELOAD="libwal_3dxp.so" mangohud --dlsym ./main
