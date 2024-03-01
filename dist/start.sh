c6438c6cdc233f08ebaaf6ac694e4ce9f25bd308
BroGamer <64546358+BroGamer4256@users.noreply.github.com>
2024-03-01 18:24:05 -0500

    Recompile shaders on boot for non-nvidia GPUs

--- dist/start.sh
+++ dist/start.sh
@@ -6,6 +6,7 @@ mkdir -p ./tmp/data/ranking/
 mkdir -p ./tmp/data/target/
 cp ./data/sound/bgm/maxi3/sys_04.wav ./tmp/sys_04.wav
 
+# Remove old C++ libs
 if [ -f ./libso/libstdc++.so.6 ]; then 
 	mv ./libso/libstdc++.so.6 ./libso/libstdc++.so.6.bak
 fi
@@ -19,6 +20,24 @@ if [ -f ./libso/libz.so.1 ]; then
 	mv ./libso/libz.so.1 ./libso/libz.so.1.bak
 fi
 
+# Recompile shaders for non-nvidia cards
+if [ ! -f ./data/shader/.recompiled ]; then
+	touch ./data/shader/.recompiled
+	for f in ./data/shader/*.cg; do
+		file=${f%.cg}
+		fp="${file}.fp"
+		vp="${file}.vp"
+		if [ -f $fp ]; then
+			rm $fp
+		fi
+		if [ -f $vp ]; then
+			rm $vp
+		fi
+		cgc -profile arbfp1 $f -entry p_main -o $fp
+		cgc -profile arbvp1 $f -entry v_main -o $vp
+	done
+fi
+
 export LD_LIBRARY_PATH="${PWD};${PWD}/libso"
-export MANGOHUD_CONFIG="fps_limit=60;no_display=1"
+export MANGOHUD_CONFIG="fps_limit=60;"
 LD_PRELOAD="libwal_3dxp.so" mangohud --dlsym ./main
