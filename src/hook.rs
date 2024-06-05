29ecedfe91f310918f0763f333092383b3d5db04
BroGamer <64546358+BroGamer4256@users.noreply.github.com>
2024-06-05 14:46:36 +1200

    Un-export hook.rs functions

--- src/hook.rs
+++ src/hook.rs
@@ -1,14 +1,11 @@
-#![allow(improper_ctypes_definitions)]
 use libc::*;
 use std::ffi::CString;
 
-#[no_mangle]
-pub unsafe extern "C" fn hook_symbol(symbol: &str, func: *const ()) -> *const () {
+pub unsafe fn hook_symbol(symbol: &str, func: *const ()) -> *const () {
 	hook(get_symbol(symbol), func)
 }
 
-#[no_mangle]
-pub unsafe extern "C" fn get_symbol(symbol: &str) -> *mut () {
+pub unsafe fn get_symbol(symbol: &str) -> *mut () {
 	let symbol = CString::new(symbol).unwrap();
 	let module = dlopen(std::ptr::null(), RTLD_LAZY);
 	let address = dlsym(module, symbol.as_ptr());
@@ -16,8 +13,7 @@ pub unsafe extern "C" fn get_symbol(symbol: &str) -> *mut () {
 	address as *mut ()
 }
 
-#[no_mangle]
-pub unsafe extern "C" fn hook(address: *mut (), func: *const ()) -> *const () {
+pub unsafe fn hook(address: *mut (), func: *const ()) -> *const () {
 	let hook = retour::RawDetour::new(address, func).unwrap();
 	hook.enable().unwrap();
 	let trampoline = hook.trampoline() as *const ();
@@ -25,8 +21,7 @@ pub unsafe extern "C" fn hook(address: *mut (), func: *const ()) -> *const () {
 	trampoline
 }
 
-#[no_mangle]
-pub unsafe extern "C" fn write_memory(address: *mut (), data: &[u8]) {
+pub unsafe fn write_memory(address: *mut (), data: &[u8]) {
 	region::protect(address, data.len(), region::Protection::READ_WRITE_EXECUTE).unwrap();
 	std::ptr::copy_nonoverlapping(data.as_ptr(), address as *mut u8, data.len());
 }
